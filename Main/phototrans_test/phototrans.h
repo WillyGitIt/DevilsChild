e27802dcb12d78a6dfde934873eae8111002743e
Yige <yigecao@gmail.com>
2018-04-13 16:55:15 +1200

    new code only using 4 transistors

--- Main/phototrans_test/phototrans.h
+++ Main/phototrans_test/phototrans.h
@@ -6,21 +6,17 @@
 class Phototrans {
     public:
       Phototrans();
-        void setup(int pinf5, int pinf3, int pinfs, int pinb5, int pinb3, int pinbs, float alpha);
+        void setup(int pinf5, int pinf3, int pinb5, int pinb3, float alpha);
         bool isFire(); 
     private:
         float avgtf5;
         float avgtf3;
-        float avgtfs;
         float avgtb5;
         float avgtb3;
-        float avgtbs;
         int pinf5;
         int pinf3;
-        int pinfs;
         int pinb5;
         int pinb3;
-        int pinbs;
         float alpha;
         
 

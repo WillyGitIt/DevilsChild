4d2dafa28fd63b99d548d82e6d92b3f10fe3b44a
Yige <yigecao@gmail.com>
2018-04-26 16:36:46 +1200

    MPU stuff moved to main file

--- Main/phototrans_test/phototrans.cpp
+++ Main/phototrans_test/phototrans.cpp
@@ -41,7 +41,7 @@ bool Phototrans::isFire() {
     float percent_diff_t5 = 100.0*(avgtf5 - avgtb5)/((avgtf5 + avgtb5)/2);
     float percent_diff_t3 = 100.0*(avgtf3 - avgtb3)/((avgtf3 + avgtb3)/2);
 
-    if (abs(percent_diff_t5) >110 && abs(percent_diff_t3) >110) 
+    if (percent_diff_t5 >110 && percent_diff_t3 >110) 
     {
         isFire = true;
     } else 
